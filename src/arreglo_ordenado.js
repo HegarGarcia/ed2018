7d93ff13f59e8337b917eae32c9b815655883ffa
Hegar Garcia <hegargarcia@gmail.com>
2018-09-17 16:54:20 -0500

    Fixing Eslint problems and Exporting Class

--- src/arreglo_ordenado.js
+++ src/arreglo_ordenado.js
@@ -1,18 +1,16 @@
-// 180904ED_arreglo_ordenado
-
-class Arreglo {
+class ArregloOrdenado {
   constructor(length) {
     this.length = length;
     this.array = [];
   }
 
   search(value = null) {
-    const length = this.array.length;
+    const { length } = this.array;
     const arr = this.array;
     let index = 0;
 
     while (index <= length && arr[index] < value) {
-      index++;
+      index += 1;
     }
 
     const found = !(index >= length || arr[index] > value);
@@ -22,16 +20,16 @@ class Arreglo {
 
   insert(value = null) {
     if (this.array.length >= this.length) {
-      throw new Error("No hay espacio disponible");
+      throw new Error('No hay espacio disponible');
     }
 
     const { index, found } = this.search(value);
 
     if (found) {
-      throw new Error("El valor ya está en el arreglo");
+      throw new Error('El valor ya está en el arreglo');
     }
 
-    for (let i = this.array.length; i > index; i--) {
+    for (let i = this.array.length; i > index; i -= 1) {
       this.array[i] = this.array[i - 1];
     }
 
@@ -42,20 +40,20 @@ class Arreglo {
 
   delete(value = null) {
     if (this.array.length === 0) {
-      throw new Error("El arreglo está vacio");
+      throw new Error('El arreglo está vacio');
     }
 
     const { index, found } = this.search(value);
 
     if (!found) {
-      throw new Error("El valor no está en el arreglo");
+      throw new Error('El valor no está en el arreglo');
     }
 
-    for (let i = index; i < this.array.length; i++) {
+    for (let i = index; i < this.array.length; i += 1) {
       this.array[i] = this.array[i + 1];
     }
 
-    this.array.length--;
+    this.array.length -= 1;
 
     return this;
   }
@@ -70,7 +68,9 @@ class Arreglo {
       this.array = tempArray;
       throw error;
     }
-    
+
     return this;
   }
 }
+
+module.exports = ArregloOrdenado;
