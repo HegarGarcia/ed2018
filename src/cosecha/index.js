e114ee441ea04c618e4d19c260ae5bf4e4bd11a5
Hegar Garcia <hegargarcia@gmail.com>
2018-09-19 08:26:17 -0500

    Adding constructor

--- src/cosecha/index.js
+++ src/cosecha/index.js
@@ -1,5 +1,15 @@
 class Cosecha {
-  constructor() {}
+  constructor(data = []) {
+    if (!Array.isArray(data)) {
+      throw new Error('Argumento no es arreglo');
+    }
+
+    if (data.length > 12 || data.length < 12) {
+      throw new Error('Cantidad de datos no válida');
+    }
+
+    this.data = data;
+  }
 
   average() {}
 
