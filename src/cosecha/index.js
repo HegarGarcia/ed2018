43b2cfc83a636b5e6b9c4194a4a83ac821cefaa2
Hegar Garcia <hegargarcia@gmail.com>
2018-09-19 10:20:36 -0500

    Finishing Cosecha

--- src/cosecha/index.js
+++ src/cosecha/index.js
@@ -9,13 +9,70 @@ class Cosecha {
     }
 
     this.data = data;
+    this.months = new Map([
+      [0, 'enero'],
+      [1, 'febrero'],
+      [2, 'marzo'],
+      [3, 'abril'],
+      [4, 'mayo'],
+      [5, 'junio'],
+      [6, 'julio'],
+      [7, 'agosto'],
+      [8, 'septiembre'],
+      [9, 'octubre'],
+      [10, 'noviembre'],
+      [11, 'diciembres'],
+    ]);
   }
 
-  average() {}
+  average() {
+    return (
+      this.data.reduce((total, month) => total + month, 0) / this.data.length
+    );
+  }
 
-  max() {}
+  find(callback) {
+    const value = this.data.reduce(callback);
+    const index = this.data.findIndex(month => month === value);
+    const month = this.months.get(index);
 
-  min() {}
+    return { month, value };
+  }
+
+  max() {
+    return this.find(
+      (pastMonth, month) => (pastMonth > month ? pastMonth : month),
+    );
+  }
+
+  min() {
+    return this.find(
+      (pastMonth, month) => (pastMonth < month ? pastMonth : month),
+    );
+  }
+
+  getStadistics() {
+    const average = this.average();
+    const emptyStadistics = {
+      overAverage: new Map(),
+      underAverage: new Map(),
+      onPoint: new Map(),
+      average,
+    };
+    const stadistics = this.data.reduce((results, month, index) => {
+      const key = this.months.get(index);
+      if (month === results.average) {
+        results.onPoint.set(key, month);
+      } else if (month > results.average) {
+        results.overAverage.set(key, month);
+      } else {
+        results.underAverage.set(key, month);
+      }
+      return results;
+    }, emptyStadistics);
+
+    return stadistics;
+  }
 }
 
 module.exports = Cosecha;
