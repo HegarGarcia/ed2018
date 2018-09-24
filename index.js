e3ad017cb6dcee5bebf2ffb00f64213969fb3261
Hegar Garcia <hegargarcia@gmail.com>
2018-09-23 22:11:59 -0500

    Adding module to REPL

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const ArregloOrdenado = require('./src/arreglo_ordenado');
 const Matrix = require('./src/arreglo_bidimensional');
 const Cosecha = require('./src/cosecha');
 const Grades = require('./src/calificaciones');
+const Costs = require('./src/production_costs');
 
 const replServer = repl.start({
   prompt: '$ ',
@@ -14,3 +15,4 @@ replServer.context.Arreglo = Arreglo;
 replServer.context.ArregloOrdenado = ArregloOrdenado;
 replServer.context.Cosecha = Cosecha;
 replServer.context.Grades = Grades;
+replServer.context.Costs = Costs;
