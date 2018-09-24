7f7599d1eb02a66127698bf64e534b98de9594b5
Hegar Garcia <hegargarcia@gmail.com>
2018-09-24 18:38:02 -0500

    Fixing linting

--- src/cosecha/index.js
+++ src/cosecha/index.js
@@ -1,3 +1,18 @@
+const months = new Map([
+  [0, 'enero'],
+  [1, 'febrero'],
+  [2, 'marzo'],
+  [3, 'abril'],
+  [4, 'mayo'],
+  [5, 'junio'],
+  [6, 'julio'],
+  [7, 'agosto'],
+  [8, 'septiembre'],
+  [9, 'octubre'],
+  [10, 'noviembre'],
+  [11, 'diciembres']
+]);
+
 class Cosecha {
   constructor(data = Array(12)) {
     if (!Array.isArray(data)) {
@@ -9,20 +24,6 @@ class Cosecha {
     }
 
     this.data = data;
-    this.months = new Map([
-      [0, 'enero'],
-      [1, 'febrero'],
-      [2, 'marzo'],
-      [3, 'abril'],
-      [4, 'mayo'],
-      [5, 'junio'],
-      [6, 'julio'],
-      [7, 'agosto'],
-      [8, 'septiembre'],
-      [9, 'octubre'],
-      [10, 'noviembre'],
-      [11, 'diciembres'],
-    ]);
   }
 
   average() {
@@ -34,20 +35,20 @@ class Cosecha {
   find(callback) {
     const value = this.data.reduce(callback);
     const index = this.data.findIndex(month => month === value);
-    const month = this.months.get(index);
+    const month = months.get(index);
 
     return { month, value };
   }
 
   max() {
     return this.find(
-      (pastMonth, month) => (pastMonth > month ? pastMonth : month),
+      (pastMonth, month) => (pastMonth > month ? pastMonth : month)
     );
   }
 
   min() {
     return this.find(
-      (pastMonth, month) => (pastMonth < month ? pastMonth : month),
+      (pastMonth, month) => (pastMonth < month ? pastMonth : month)
     );
   }
 
@@ -57,10 +58,10 @@ class Cosecha {
       overAverage: new Map(),
       underAverage: new Map(),
       onPoint: new Map(),
-      average,
+      average
     };
     const stadistics = this.data.reduce((results, month, index) => {
-      const key = this.months.get(index);
+      const key = months.get(index);
       if (month === results.average) {
         results.onPoint.set(key, month);
       } else if (month > results.average) {
