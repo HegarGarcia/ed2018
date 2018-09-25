d3de10043811d69452d0ad5e7eb721f2f7d14b80
Hegar Garcia <hegargarcia@gmail.com>
2018-09-25 14:12:35 -0500

    Bug fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 const { random } = require('faker');
 
-function mockArray(length, { max = 10, min = 1 }) {
+function mockArray(length, { max = 10, min = 1 } = {}) {
   return Array.from({ length }, () => random.number({ max, min }));
 }
 
