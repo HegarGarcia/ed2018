c7df0b0228cea1f13b2b8ad19db7ecae79513a3f
Hegar Garcia <hegargarcia@gmail.com>
2018-09-28 14:56:48 -0500

    Adding Math Module to REPL

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const repl = require('repl');
-const Arreglo = require('./src/arreglos_desordenado');
-const ArregloOrdenado = require('./src/arreglo_ordenado');
+const Arreglo = require('./src/arrays/arreglos_desordenado');
+const ArregloOrdenado = require('./src/arrays/arreglo_ordenado');
 const { Matrix } = require('./src/matrix');
 const Cosecha = require('./src/cosecha');
 const Grades = require('./src/calificaciones');
 const Costs = require('./src/production_costs');
 const Stack = require('./src/stacks');
+const { toPostfix } = require('./src/math');
 
 const replServer = repl.start({
   prompt: '$ '
@@ -18,3 +19,4 @@ replServer.context.Cosecha = Cosecha;
 replServer.context.Grades = Grades;
 replServer.context.Costs = Costs;
 replServer.context.Stack = Stack;
+replServer.context.toPostfix = toPostfix;
