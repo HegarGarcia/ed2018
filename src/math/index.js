9afe7026e50e828ca9d320d07b955204b78b2795
Hegar Garcia <hegargarcia@gmail.com>
2018-09-28 14:57:50 -0500

    Adding index to Math Module

---
+++ src/math/index.js
@@ -0,0 +1,9 @@
+const { toInfix } = require('./infix');
+const { toPostfix } = require('./postfix');
+const { toPrefix } = require('./prefix');
+
+module.exports = {
+  toInfix,
+  toPostfix,
+  toPrefix
+};
