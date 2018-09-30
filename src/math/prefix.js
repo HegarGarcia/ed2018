3ca15c4f73b4e7dc6574666c2dd66540426e40f9
Hegar Garcia <hegargarcia@gmail.com>
2018-09-30 15:11:32 -0500

    Adding toPrefix functionality

---
+++ src/math/prefix.js
@@ -0,0 +1,57 @@
+const Stack = require('../stacks');
+const { operators, getPriority } = require('./lib');
+
+function toPrefix(expression = '') {
+  expression = [...expression.replace(/\s/g, '')].reverse().join('');
+  const validExpression =
+    expression && typeof expression === 'string' && expression !== '';
+
+  if (!validExpression) {
+    throw new Error('Expression invalid');
+  }
+
+  const expressionLength = expression.length;
+  const expressionStack = new Stack(expressionLength, 'string');
+  const isAlpha = /^[a-z0-9]+$/i;
+  let result = '';
+
+  for (const character of expression) {
+    if (character === ')') {
+      expressionStack.push(character);
+    } else if (character === '(') {
+      let current = expressionStack.pop();
+
+      while (current !== ')') {
+        result += current;
+        current = expressionStack.pop();
+      }
+    } else if (isAlpha.test(character)) {
+      result += character;
+    } else if (operators.includes(character)) {
+      let operator;
+      let topIndex = expressionStack.top - 1;
+      const priority = getPriority(expressionStack.stack[topIndex], character);
+
+      while (
+        expressionStack.top > 0 &&
+        operators.includes(expressionStack.stack[topIndex]) &&
+        ['<'].includes(priority)
+      ) {
+        operator = expressionStack.pop();
+        topIndex = expressionStack.top - 1;
+        result += operator;
+      }
+      expressionStack.push(character);
+    } else {
+      expressionStack.push(character);
+    }
+  }
+
+  while (expressionStack.top > 0) {
+    result += expressionStack.pop();
+  }
+
+  return [...result].reverse().join('');
+}
+
+module.exports = { toPrefix };
