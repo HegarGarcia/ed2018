809fd1311a1235cfe22a31a9256276c37cab8037
Hegar Garcia <hegargarcia@gmail.com>
2018-10-02 18:26:05 -0500

    Fixing import statements

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const repl = require('repl');
-const Arreglo = require('./src/arrays/arreglos_desordenado');
-const ArregloOrdenado = require('./src/arrays/arreglo_ordenado');
-const { Matrix } = require('./src/matrix');
-const Cosecha = require('./src/cosecha');
-const Grades = require('./src/calificaciones');
-const Costs = require('./src/production_costs');
-const Stack = require('./src/stacks');
-const math = require('./src/math');
+const Arreglo = require('./lib/arrays/arreglos_desordenado');
+const ArregloOrdenado = require('./lib/arrays/arreglo_ordenado');
+const { Matrix } = require('./lib/matrix');
+const Cosecha = require('./lib/cosecha');
+const Grades = require('./lib/calificaciones');
+const Costs = require('./lib/production_costs');
+const Stack = require('./lib/stacks');
+const math = require('./lib/math');
 
 const replServer = repl.start({
   prompt: '$ '
